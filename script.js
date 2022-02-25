bbbe1bf33bbf73a77ea176ded2498dfc8c101199
pedrolmr <pedroluismontesinosrivera@gmail.com>
2022-02-24 22:45:41 -0500

    image blur in js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-script.js;
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (value, low1, high1, low2, high2) => {
+  return ((value - low1) * (high2 - low2)) / (high1 - low1) + low2;
+};
